e68c52bf9ca72a219d6a9d496ccd1b85f8fe4de6
Ed Warnicke <hagbard@gmail.com>
2020-06-02 14:02:38 -0500

    Fix go generate and its check
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- internal/imports/imports.go
+++ internal/imports/imports.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/kelseyhightower/envconfig"
 	_ "github.com/networkservicemesh/sdk-vppagent/pkg/networkservice/chains/xconnectns"
 	_ "github.com/networkservicemesh/sdk-vppagent/pkg/tools/vppagent"
+	_ "github.com/networkservicemesh/sdk/pkg/networkservice/common/authorize"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/debug"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/log"
