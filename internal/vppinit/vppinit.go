b955c590d7ffd9b57236ae7396473d9576c0603f
Denis Tingajkin <denis.tingajkin@xored.com>
2020-08-22 10:53:33 +0700

    add !windows build tags
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- internal/vppinit/vppinit.go
+++ internal/vppinit/vppinit.go
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build !windows
+
 // Package vppinit provides a vppinit function for use with sdk-vppagent
 package vppinit
 
